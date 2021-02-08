ef1407fe70cbf325b5f27596f3fd1897829cc6e3
Caique Pimenta <cpimenta.apsoftware@gmail.com>
2021-02-07 23:54:51 -0300

    fix: uso correto do envalid

--- src/pages/CriarOrfanato.tsx
+++ src/pages/CriarOrfanato.tsx
@@ -19,6 +19,8 @@ import mapIcon from '../utils/mapIcon';
 import localizacaoPadrao from "../utils/localizacaoPadrao";
 import api from "../services/api";
 
+import { environmentVariables } from './../utils/validateEnv';
+
 
 // const happyMapIcon = L.icon({
 //   iconUrl: mapMarkerImg,
@@ -130,7 +132,7 @@ export default function CriarOrfanato() {
               onclick={criarMarcadorMapa}
             >
               <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${environmentVariables.REACT_APP_MAPBOX_TOKEN}`}
               />
 
               {/* <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} /> */}
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,11 +1,36 @@
 import {
-  cleanEnv, port, str,
+  cleanEnv, email, EnvError, EnvMissingError, json, makeValidator, str, testOnly,
 } from 'envalid';
 
-function validateEnv() {
-  cleanEnv(process.env, {
-      REACT_APP_MAPBOX_TOKEN: str(),      
-  });
-}
+const duasLetras = makeValidator(x => {
+  if (/^[A-Za-z]{2}$/.test(x)) return x.toUpperCase()
+  else throw new Error('Esperado duas letras')
+});
 
-export default validateEnv; 
+export const environmentVariables = cleanEnv(process.env, {
+  REACT_APP_MAPBOX_TOKEN: str({ desc: 'token de acesso à API do MAPBOX' }),
+  ADMIN_EMAIL: email({ default: 'cpimenta.apsoftware@gmail.com' }),
+  EMAIL_CONFIG_JSON: json({ desc: 'parâmetros adicionais do e-mail'}),
+  NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
+  INICIAIS: duasLetras({ devDefault: testOnly('CM') })
+},
+  {
+    reporter: ({ errors, env }) => {      
+      console.log(Object.values(errors));
+      console.log('Variáveis de ambiente inválidas: ' + Object.keys(errors));
+      Object.values(errors).forEach(err => {
+        if (err instanceof EnvError) {
+          console.log('EnvError: ' + err.message);
+        } else if (err instanceof EnvMissingError) {
+          console.log('EnvMissingError: ' + err.message);
+        } else if (err instanceof TypeError) {
+          console.log('TypeError: ' + err.message);
+        } else {
+          console.log('Outros: ' + err);
+        }
+      });
+    }
+  }
+);
+
+export type EnvironmentVariables = typeof environmentVariables;
